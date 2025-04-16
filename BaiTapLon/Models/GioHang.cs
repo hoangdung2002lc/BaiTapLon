fbe1c3ccf08f70df13b8e27ef85a585fc161d0ff
Onlap.vn <hoangdung2002lc@gmail.com>
2025-04-16 11:15:18 +0700

    Thêm file vào nhánh develop

---
+++ BaiTapLon/Models/GioHang.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiTapLon.Models
+{
+    public class GioHang
+    {
+        [Key]
+        public int ID { get; set; }
+        public int NguoiDungID { get; set; }
+        public int PetID { get; set; }
+        public NguoiDung? NguoiDung { get; set;}
+        public Pet? Pet { get; set; }
+    }
+}
